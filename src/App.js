772b18dbdb8a7fa17c6112d5ad0383777b6c006b
wutong21 <18072916289@163.com>
2023-02-26 11:28:39 +0800

    init react todolist project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+// @ts-nocheck
+import { useEffect, useRef, useState } from "react";
+import TodoList from "./TodoList";
+import { v4 } from "uuid"
+
+const LOCAL_STORAGE_KET = 'todoApp.todos'
 
 function App() {
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+
+  function toggleTodo(id) {
+    const newTodos = [...todos]
+    let todo = newTodos.find(item => item.id === id)
+    todo.complete = !todo.complete
+    setTodos(newTodos)
+  }
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KET))
+    if (storedTodos) {
+      setTodos(prevTodos => {
+        return [...prevTodos, ...storedTodos]
+      })
+    }
+  }, [])
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KET, JSON.stringify(todos))
+  }, [todos])
+
+  function handleAddTodo(e) {
+    let name = todoNameRef.current.value
+    if (name === '') return
+    todoNameRef.current.value = null
+    setTodos(prevTodos => {
+      return [...prevTodos, { id: v4(), name: name, complete: false }]
+    })
+  }
+  function handleClearTodo() {
+    setTodos([])
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <> <TodoList todos={todos} toggleTodo={toggleTodo} />
+      <input ref={todoNameRef} type="text" />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <button onClick={handleClearTodo}>Clear Completed todos</button>
+      <div>{todos.filter(item => !item.complete).length} left to do</div>
+    </>
+  )
 }
 
 export default App;
